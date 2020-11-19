dbd129754ab19086ac4f52d2cbb9932458219de8
Geoffrey Duplessi <grezor.gamer@gmail.com>
2020-11-19 15:52:43 +0100

    add image profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {Typography} from '@material-ui/core'
+import './Profile.css'
+import Background from '../../assets/images/profile.jpg';
 
 const Profile = () => {
   return (
-    <div>
-      this is the profile
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className='name'>Geoffrey Duplessi</Typography>
+        <Typography className='title'>Developer</Typography>
+      </div>
+      <figure className="profile_image">
+        <img src={Background} alt="" />
+      </figure>
+
+      <div className="profile_information">
+        Timeline
+        <br />
+        <button>My button</button>
+      </div>
     </div>
   )
 }
